c37caaa96227c9aa16536cf5dab682006e00de77
Safuan04 <elmai.safuan@gmail.com>
2024-02-01 19:27:13 +0100

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python3
-"""Defining a function validUTF8"""
+"""Define a function named validUTF8"""
 
 
 def validUTF8(data):
-    """determines if a given data set represents a
-    valid UTF-8 encoding"""
+    """Check if a given data set represents a valid UTF-8 encoding"""
 
-    number_of_next_bytes = 0
-
-    for byte in data:
-        if byte >= 255:  # the number of bits is higher than 8
-            return False
-        if number_of_next_bytes == 0:
-            if byte >> 3 == 0b11110:
-                number_of_next_bytes = 3
-            elif byte >> 4 == 0b1110:
-                number_of_next_bytes = 2
-            elif byte >> 5 == 0b1100:
-                number_of_next_bytes = 1
-            elif byte >> 7 != 0:  # if the byte's leftmost is 1 (starts with 1)
-                # and none of the above conditions are true
-                # it is definitly not a valid utf8
+    # Iterate through the data
+    i = 0
+    while i < len(data):
+        # Check if the most significant bit (MSB) is 0
+        if (data[i] & 0b10000000) == 0:
+            # If MSB is 0, it's a single-byte character (ASCII)
+            if data[i] <= 128:
+                i += 1
+                continue
+            else:
                 return False
-        # checking the next n-bytes
         else:
-            # if the next byte does not start with '10' '0b10'
-            if byte >> 6 != 0b10:
-                return False
-            number_of_next_bytes -= 1
+            # If MSB is 1, it's the start of a multi-byte character
+            # Determine the number of bytes for this character
+            num_bytes = 0
+            while (data[i] & (0b10000000 >> num_bytes)) != 0:
+                num_bytes += 1
 
-    return number_of_next_bytes == 0
+            # Check the number of continuation bytes
+            if num_bytes < 2 or num_bytes > 4:
+                return False  # Invalid number of continuation bytes
+
+            # Check the following continuation bytes
+            for j in range(1, num_bytes):
+                i += 1
+                if i >= len(data) or (data[i] & 0b11000000) != 0b10000000:
+                    return False  # Invalid continuation byte
+
+        i += 1
+
+    return True
