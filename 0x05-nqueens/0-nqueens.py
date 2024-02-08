247f43fc877e0f0c1c7033661998d3012e1ff99d
Safuan04 <elmai.safuan@gmail.com>
2024-02-08 22:46:17 +0100

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,66 +1,57 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Solving the nqueens Challenge"""
+from sys import argv, exit
 
-"""Solves the N-Queens problem."""
 
-import sys
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+try:
+    N = int(argv[1])
+except ValueError:
+    print('N must be a number')
+    exit(1)
+
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
 
 
 def n_queens(N: int):
-    """Returns all possible solutions for N queens in an NxN board."""
+    """returns all possible solutions for N queens in NxN board"""
     columns = set()
-    positive_diag = set()
-    negative_diag = set()
+    positiveDiag = set()
+    negativeDiag = set()
     result = []
 
     def backtrack(row: int, queens: list):
-        """Backtracking algorithm to find possible solutions."""
+        """backtracking algorithm to find possible solutions"""
         if row == N:
             result.append(queens.copy())
             return
         for col in range(N):
-            # If placing a queen at (row, col) violates any constraints
-            if (
-                col in columns
-                or (row - col) in negative_diag
-                or (row + col) in positive_diag
-            ):
+            # if placing a queen at (row, col) violates any constraints
+            if (col in columns
+                    or (row - col) in negativeDiag
+                    or (row + col) in positiveDiag):
                 continue
             # If placing a queen at (row, col) is valid:
             columns.add(col)
-            negative_diag.add(row - col)
-            positive_diag.add(row + col)
+            negativeDiag.add(row - col)
+            positiveDiag.add(row + col)
             queens.append([row, col])
             backtrack(row + 1, queens)
             # Undoing the changes for the next iteration
             columns.remove(col)
-            negative_diag.remove(row - col)
-            positive_diag.remove(row + col)
+            negativeDiag.remove(row - col)
+            positiveDiag.remove(row + col)
             queens.pop()
-
     backtrack(0, [])
     return result
 
 
-def main():
-    """Main function."""
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
+if __name__ == "__main__":
     solutions = n_queens(N)
     for solution in solutions:
         print(solution)
-
-
-if __name__ == "__main__":
-    main()
