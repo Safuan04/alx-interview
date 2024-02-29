803d9b890ca664c703ea4b09bdeaa2a94754a313
Safuan04 <elmai.safuan@gmail.com>
2024-02-29 21:41:30 +0100

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Defining makeChange function"""
+
 
 def makeChange(coins, total):
+    """ Given a pile of coins of different values,
+    -   determine the fewest number of coins needed
+    -   to meet a given amount total
+    """
     if total <= 0:
         return 0
 
